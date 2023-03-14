40fa7bf9a3c555cb957eae3e4a36d4b060d6f941
qasim-rafique-malik <qasim.online.now@gmail.com>
2023-03-14 17:05:12 +0500

    new updates

--- wp-content/themes/buildpro-child/custom/js/custom.js
+++ wp-content/themes/buildpro-child/custom/js/custom.js
@@ -2,19 +2,35 @@
 jQuery(document).ready(function($) {
 
 
-    const subjectOption = $('#getAQuoteFrom #subjectOption');
-    const otherSubjectField = $("#getAQuoteFrom #otherSubject");
-    otherSubjectField.hide();
-    subjectOption.on('change', function() {
+    const GQSubjectOption = $('#getAQuoteFrom #subjectOption');
+    const GQOtherSubjectField = $("#getAQuoteFrom #otherSubject");
+    GQOtherSubjectField.hide();
+    GQSubjectOption.on('change', function() {
         if(this.value == "Others"){
-            otherSubjectField.show();
-            subjectOption.attr('name', 'otherhide');
-            otherSubjectField.attr('name', 'subject');
+            GQOtherSubjectField.show();
+            GQSubjectOption.attr('name', 'otherhide');
+            GQOtherSubjectField.attr('name', 'subject');
 
         }else{
-            otherSubjectField.hide();
-            otherSubjectField.attr('name', 'otherhide');
-            subjectOption.attr('name', 'subject');
+            GQOtherSubjectField.hide();
+            GQOtherSubjectField.attr('name', 'otherhide');
+            GQSubjectOption.attr('name', 'subject');
+        }
+    });
+
+    const CFSubjectOption = $('#contact_form #subjectOption');
+    const CFOtherSubjectField = $("#contact_form #otherSubject");
+    CFOtherSubjectField.hide();
+    CFSubjectOption.on('change', function() {
+        if(this.value == "Others"){
+            CFOtherSubjectField.show();
+            CFSubjectOption.attr('name', 'otherhide');
+            CFOtherSubjectField.attr('name', 'subject');
+
+        }else{
+            CFOtherSubjectField.hide();
+            CFOtherSubjectField.attr('name', 'otherhide');
+            CFSubjectOption.attr('name', 'subject');
         }
     });
 });
